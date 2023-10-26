91055ee7fe2ef3953a1e43ff1b8904b9bafe2ab6
Muhammad Arslan Arshad <109333063+Arslanlahori@users.noreply.github.com>
2023-10-26 22:47:07 +0500

    Many to One relationship in django between three table Student,Subject,StudentClass

---
+++ Many_to_one/manytoone/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+
+from manytoone.models import Student,Subject,Studentclass
+
+# Register your models here.
+
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['FirstName', 'LastName', 'DateOfBirth', 'Gender', 'DateOfAdmission']
+   
+admin.site.register(Student,StudentAdmin)
+
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ['SubjectName', 'SubDescription', 'student']
+
+admin.site.register(Subject,SubjectAdmin)
+
+
+class StudentclassAdmin(admin.ModelAdmin):
+    list_display = ['ClassName', 'ClassDescription', 'student']
+
+
+admin.site.register(Studentclass,StudentclassAdmin)
---
+++ Many_to_one/manytoone/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+#Many to One relation 
+
+class Student(models.Model):
+    FirstName=models.CharField(max_length=30)
+    LastName=models.CharField(max_length=30)
+    DateOfBirth=models.DateField()
+    Gender=models.CharField(max_length=10)
+    DateOfAdmission=models.DateField()
+
+
+class Subject(models.Model):
+    SubjectName=models.CharField(max_length=70)
+    SubDescription=models.TextField()
+    student=models.ForeignKey(Student,on_delete=models.CASCADE)
+    #student=models.ForeignKey(Student,on_delete=models.PROTECT)
+
+class Studentclass(models.Model):
+    ClassName=models.CharField(max_length=60)
+    ClassDescription=models.TextField()
+    student=models.ForeignKey(Student,on_delete=models.CASCADE)
+    #student=models.ForeignKey(Student,on_delete=models.PROTECT)
